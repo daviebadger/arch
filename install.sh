f778f9ef049c08087dac1d06883dd3af0037e22e
Davie Badger <davie.badger@gmail.com>
2020-05-17 20:23:42 +0200

    Add partitioning

--- install.sh
+++ install.sh
@@ -12,3 +12,19 @@ ping -c 1 archlinux.org
 
 timedatectl set-ntp true
 timedatectl set-timezone Europe/Prague
+
+DISK="/dev/$(lsblk | awk '/disk/ {print $1}')"
+MEMORY_GB="$(free --giga | awk '/Mem:/ {print $2}')"
+SWAP_PARTITION_END_MIB="$((261 + "${MEMORY_GB}" * 1000))Mib"
+
+PARTED_COMMANDS=(
+  "mklabel gpt"
+  "mkpart primary fat32 1Mib 261Mib"
+  "set 1 esp on"
+  "mkpart primary linux-swap 261Mib ${SWAP_PARTITION_END_MIB}"
+  "mkpart primary ext4 ${SWAP_PARTITION_END_MIB} 100%"
+)
+
+for parted_command in "${PARTED_COMMANDS[@]}"; do
+  parted --script "${DISK}" "${parted_command}"
+done
