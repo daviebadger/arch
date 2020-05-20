4ab2cfbc5140250a4f8801b7222a1bdcb80f9db2
Davie Badger <davie.badger@gmail.com>
2020-05-20 22:10:50 +0200

    Generate locale

--- chroot.sh
+++ chroot.sh
@@ -4,3 +4,8 @@
 
 ln -sf /usr/share/zoneinfo/Europe/Prague /etc/localtime
 hwclock --systohc
+
+sed -i "s/#cs_CZ.UTF-8/cs_CZ.UTF-8/" /etc/locale.gen
+sed -i "s/#en_US.UTF-8/en_US.UTF-8/" /etc/locale.gen
+
+locale-gen
