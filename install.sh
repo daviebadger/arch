fe7616fb54e0ebf6526bd14d9e81912aecdb8346
Davie Badger <davie.badger@gmail.com>
2020-05-28 23:15:03 +0200

    Make partitions conditionally for gpt or mbr

--- install.sh
+++ install.sh
@@ -15,36 +15,63 @@ timedatectl set-timezone Europe/Prague
 
 DISK="/dev/$(lsblk | awk '/disk/ {print $1}')"
 MEMORY_GB="$(free --giga | awk '/Mem:/ {print $2}')"
-SWAP_PARTITION_END_MIB="$((261 + "${MEMORY_GB}" * 1000))Mib"
 
-PARTED_COMMANDS=(
-  "mklabel gpt"
-  "mkpart primary fat32 1Mib 261Mib"
-  "set 1 esp on"
-  "mkpart primary linux-swap 261Mib ${SWAP_PARTITION_END_MIB}"
-  "mkpart primary ext4 ${SWAP_PARTITION_END_MIB} 100%"
-)
+if ls /sys/firmware/efi/efivars; then
+  SWAP_PARTITION_END_MIB="$((261 + "${MEMORY_GB}" * 1000))Mib"
 
-for parted_command in "${PARTED_COMMANDS[@]}"; do
-  parted --script "${DISK}" "${parted_command}"
-done
+  PARTED_COMMANDS=(
+    "mklabel gpt"
+    "mkpart primary fat32 1Mib 261Mib"
+    "set 1 esp on"
+    "mkpart primary linux-swap 261Mib ${SWAP_PARTITION_END_MIB}"
+    "mkpart primary ext4 ${SWAP_PARTITION_END_MIB} 100%"
+  )
 
-EFI_PARTITION="${DISK}1"
+  for parted_command in "${PARTED_COMMANDS[@]}"; do
+    parted --script "${DISK}" "${parted_command}"
+  done
 
-mkfs.fat -F 32 "${EFI_PARTITION}"
+  EFI_PARTITION="${DISK}1"
 
-SWAP_PARTITION="${DISK}2"
+  mkfs.fat -F 32 "${EFI_PARTITION}"
 
-mkswap "${SWAP_PARTITION}"
-swapon "${SWAP_PARTITION}"
+  SWAP_PARTITION="${DISK}2"
 
-ROOT_PARTITION="${DISK}3"
+  mkswap "${SWAP_PARTITION}"
+  swapon "${SWAP_PARTITION}"
 
-mkfs.ext4 "${ROOT_PARTITION}"
+  ROOT_PARTITION="${DISK}3"
 
-mount "${ROOT_PARTITION}" /mnt
-mkdir /mnt/efi
-mount "${EFI_PARTITION}" /mnt/efi
+  mkfs.ext4 "${ROOT_PARTITION}"
+
+  mount "${ROOT_PARTITION}" /mnt
+  mkdir /mnt/efi
+  mount "${EFI_PARTITION}" /mnt/efi
+else
+  SWAP_PARTITION_END_MIB="$((1 + "${MEMORY_GB}" * 1000))Mib"
+
+  PARTED_COMMANDS=(
+    "mklabel msdos"
+    "mkpart primary linux-swap 1Mib ${SWAP_PARTITION_END_MIB}"
+    "mkpart primary ext4 ${SWAP_PARTITION_END_MIB} 100%"
+    "set 2 boot on"
+  )
+
+  for parted_command in "${PARTED_COMMANDS[@]}"; do
+    parted --script "${DISK}" "${parted_command}"
+  done
+
+  SWAP_PARTITION="${DISK}1"
+
+  mkswap "${SWAP_PARTITION}"
+  swapon "${SWAP_PARTITION}"
+
+  ROOT_PARTITION="${DISK}2"
+
+  mkfs.ext4 "${ROOT_PARTITION}"
+
+  mount "${ROOT_PARTITION}" /mnt
+fi
 
 pacstrap /mnt base base-devel linux linux-firmware
 
